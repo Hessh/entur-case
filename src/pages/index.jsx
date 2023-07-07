38ea5a63b0cda7d9c546d9f325054c3a7b8eb60f
Haakon Hess <haakon.hess@gmail.com>
2023-07-07 15:20:33 +0200

    Removed searchbutton

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
-import Layout from '@/components/Layout/Layout'
+import { useEffect, useState } from 'react'
 import axios from 'axios';
-import { useState } from 'react'
+
+import Layout from '@/components/Layout/Layout'
 
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -8,35 +9,38 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  console.log(results)
-
-  const handleSearch = async () => {
+  const handleSearch = async (value) =>{
+    setSearchTerm(value);
     setLoading(true);
     setError(null);
 
     try {
-      const response = await axios.get('/api/location', {
-      params: {
-        searchTerm: searchTerm,
-      },
-    });
+      if(value.length > 0) {
+        const response = await axios.get('/api/location', {
+          params: {
+            searchTerm: value,
+          },
+        });
 
-      if (response.status !== 200) {
-        throw new Error('Error fetching data');
+        if (response.status !== 200) {
+          throw new Error('Error fetching data');
+        }
+
+        const data = await response.data;
+        const results = data.features.filter(result => result.properties.layer === 'venue');
+        setResults(results);
+      } else {
+        setResults([]);
       }
-
-      const data = await response.data;
-      const results = data.features.filter(result => result.properties.layer === 'venue');
-      setResults(results);
     } catch (error) {
       setError(error.message);
     } finally {
       setLoading(false);
     }
-  };
+  }
 
-  const handleSelect = async (id) => {
-    console.log(id)
+  const handleSelect = async (placeId) => {
+    console.log(placeId)
     // setLoading(true);
     // setError(null);
 
@@ -60,43 +64,44 @@ const Home = () => {
     //   setLoading(false);
     // }
   };
+
+  useEffect(() => {
+    if (error) {
+      console.log(error)
+    }
+  }, [error]);
   
   return (
     <Layout>
       <div className="container mx-auto py-8">
-        <h1 className="text-3xl font-bold mb-4">Public Transportation Departures</h1>
+        <h1 className="text-3xl font-bold mb-4">Finn avganger</h1>
         <div className="mb-4">
-          <input
-            type="text"
-            placeholder="Enter a location"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="p-2 border border-gray-300 rounded w-64"
-          />
-          <button
-            onClick={handleSearch}
-            className="px-4 py-2 ml-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            Search
-          </button>
-        </div>
+        <input
+          type="text"
+          placeholder="Enter a location"
+          value={searchTerm}
+          onChange={(e) => handleSearch(e.target.value)}
+          className="p-2 border border-gray-300 rounded w-64"
+        />
+      </div>
 
         {loading ? (
-          <p>Loading...</p>
+          <p>Laster inn resultater...</p>
         ) : error ? (
-          <p>Error: {error}</p>
+          <p>En feil har oppstått: {error}</p>
         ) : results ? (
           <ul>
-            {results.map((result) => (
-              <div key={result.id} className="mb-4">
-              <button key={result.id} className="" onClick={(e) => handleSelect(e.target.value)}>
-                {result.properties.name}
-              </button>
-              </div>
-            ))}
+            {results.map(({properties}) => {
+              return(
+                <div key={properties.id} className="mb-4">
+                  <button className="" onClick={() => handleSelect(properties.id)}>
+                    {properties.name}
+                  </button>
+                </div>
+            )})}
           </ul>
         ) : (
-          <p>No results found.</p>
+          <p>Fant ingen resultater.</p>
         )}
       </div>
     </Layout>
